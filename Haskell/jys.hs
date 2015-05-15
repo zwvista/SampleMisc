637be58d2ef933e6923f14f8af2eb833e8a17fa2
zwvista <zwvista@msn.com>
2015-05-15 21:56:23 +0900

    Finished jys.hs

--- Haskell/jys.hs
+++ Haskell/jys.hs
@@ -1,8 +1,24 @@
-import Data.Map
+﻿-- jys.hs
+
+import Data.Map (fromListWith, toAscList)
+import Data.List (intersperse)
 
 (|>) x f = f x
 sortAndGroup kv = fromListWith (++) [(k, [v]) | (k, v) <- kv]
-sortAndGroupBy f a = sortAndGroup $ zip (Prelude.map f a) a
+sortAndGroupBy f xs = sortAndGroup $ zip (map f xs) xs
 
-print :: String -> int -> String
-print text offset = undefined
+verticalWriting text offset = zip [0..] text
+    |> sortAndGroupBy ((`mod` offset) . fst)
+    |> toAscList
+    |> map snd 
+    |> map (map snd)
+    |> map (intersperse '|')
+    |> unlines
+    
+main = putStr $ verticalWriting "床前明月光疑是地上霜举头望明月低头思故乡" 5
+
+--低|举|疑|床
+--头|头|是|前
+--思|望|地|明
+--故|明|上|月
+--乡|月|霜|光
