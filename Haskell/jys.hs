14763fba6c24354df1fdde55d7d45f7bfe7375af
趙偉 <zwvista@msn.com>
2017-03-12 19:46:04 +0900

    elems

--- Haskell/jys.hs
+++ Haskell/jys.hs
@@ -1,17 +1,20 @@
 ﻿-- jys.hs
 
-import Data.Map (fromListWith, toAscList)
-import Data.List (intersperse)
+import Data.Map.Strict (fromListWith, elems)
+import Data.List (intercalate, groupBy, sortOn)
+import Data.Function (on)
 
 verticalWriting text offset =
---  let xss = toAscList $ fromListWith (++) [(x `mod` offset, [y]) | (x, y) <- zip [0..] text]
-    let xss = toAscList $ fromListWith (++) $ zipWith (\x y -> (x `mod` offset, [y])) [0..] text
-    in  unlines [intersperse '|' xs | (_, xs) <- xss]
+    let indexedChars = zipWith (\x y -> (x `mod` offset, [y])) [0..] text
+    in elems $ fromListWith (\a b -> a ++ "|" ++ b) indexedChars
+    -- in map (intercalate "|" . reverse . map snd) $ groupBy ((==) `on` fst) $ sortOn fst $ indexedChars
 
-main = putStr $ verticalWriting "床前明月光疑是地上霜举头望明月低头思故乡" 5
+main = mapM_ putStrLn $ verticalWriting "床前明月光疑是地上霜举头望明月低头思故乡" 5
 
---低|举|疑|床
---头|头|是|前
---思|望|地|明
---故|明|上|月
---乡|月|霜|光
+{-
+低|举|疑|床
+头|头|是|前
+思|望|地|明
+故|明|上|月
+乡|月|霜|光
+-}
