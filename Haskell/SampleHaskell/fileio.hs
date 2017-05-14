074272ea1cf9f51bc0a4a43a01b37c70d79717ea
zwvista <zwvista@msn.com>
2017-05-15 01:28:22 +0900

    fileio.hs

---
+++ Haskell/SampleHaskell/fileio.hs
@@ -0,0 +1,26 @@
+import System.IO
+import Data.Char
+
+inputPath = "input.txt"
+outputPath = "output.txt"
+openFile1 = do
+    handle <- openFile inputPath ReadMode
+    contents <- hGetContents handle
+    putStr contents
+    hClose handle
+withFile1 = do
+    withFile inputPath ReadMode (\handle -> do
+        contents <- hGetContents handle
+        putStr contents)
+readFile1 = do
+    contents <- readFile inputPath
+    putStr contents
+
+readWriteUTF8File = do
+    inputHandle <- openFile inputPath ReadMode 
+    hSetEncoding inputHandle utf8
+    theInput <- hGetContents inputHandle
+    outputHandle <- openFile outputPath WriteMode
+    hSetEncoding outputHandle utf8
+    hPutStr outputHandle $ map toUpper theInput
+    hClose outputHandle
