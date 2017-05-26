cea0ee1ea1769f5325d5c65fbded60c314e0afa6
zwvista <zwvista@msn.com>
2017-05-26 18:34:41 +0900

    smaple

--- Kotlin/SampleKotlin/src/ABC.kt
+++ Kotlin/SampleKotlin/src/ABC.kt
@@ -11,6 +11,33 @@ fun f5(n: Int) {println(n)}
 fun f6(n: Int): Unit = println(n)
 fun f7(n: Int) = println(n)
 
+fun describe(obj: Any): String =
+    when (obj) {
+        1          -> "One"
+        "Hello"    -> "Greeting"
+        is Long    -> "Long"
+        !is String -> "Not a string"
+        else       -> "Unknown"
+    }
+
+fun outside() {
+    var a = 1
+    fun inside() {
+        a++;
+    }
+    inside();
+    println(a);
+}
+
+fun add(a: Int = 1, b: Int = 2) = a + b
+fun avg(vararg numbers: Double): Double {return 0.0}
+
+class C {
+    fun foo() { println("member") }
+}
+fun C.foo() { println("extension") }
+fun C.foo(i: Int) { println("extension") }
+
 fun main(args: Array<String>) {
     println(f1(0))
     println(f2(1))
@@ -26,6 +53,29 @@ fun main(args: Array<String>) {
     for (i in 1 until 10) print(i); println() // prints "123456789"
 
     val x = 3
-    if (x in 1..10) print(x)
-    if (x !in 1..10) print(x)
+    if (x in 1..10) println(x)
+    if (x !in 1..10) println(x)
+
+    val x1 = 10; val y1 = 20
+    val s = "x=$x1, y=${y1 + 1}" // x=10, y=21
+    println(s)
+
+    val price = """
+${'$'}9.99
+"""
+    println(price)
+
+    println(describe(1));
+    println(describe(2L));
+    println(describe(3.0));
+    println(describe("abc"));
+
+    outside()
+
+    println(add());
+    println(add(3));
+    println(add(b = 3));
+
+    C().foo() // member
+    C().foo(1) // extension
 }
