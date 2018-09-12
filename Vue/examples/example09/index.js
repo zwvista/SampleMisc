b8b8686c3b482e9ca0bf8f67bc5bb3c8a3dbab57
zwvista <zwvista@msn.com>
2018-09-12 19:10:45 +0900

    index 4

--- Vue/examples/example09/index.js
+++ Vue/examples/example09/index.js
@@ -17,7 +17,7 @@ var app = new Vue({
     manualSelectedList: [],
   },
   methods: {
-    multiEnterClick: function() {
+    batchInputClick: function() {
       var value = prompt("一括入力", "");
       if (value === null) return;
       for(var i = 0; i < 48; i++) {
@@ -44,37 +44,154 @@ var app = new Vue({
         $(".td-manual:nth-child(" + (i+1) + ")").children('div').html(value);
       }
     },
+    emptyAll: function() {
+      this.priceList = [];
+      this.chargeList = [];
+      this.dischargeList = [];
+      this.autoList = [];
+      this.manualList = [];
+      this.manualOriginalList = [];
+      this.manualSelectedList = [];
+    },
+    clearAll: function() {
+      this.emptyAll();
+      for (var i = 0; i < 48; i++) {
+        this.priceList.push(null);
+        this.chargeList.push(null);
+        this.dischargeList.push(null);
+        this.autoList.push(null);
+        this.manualList.push(null);
+        this.manualOriginalList.push(null);
+        this.manualSelectedList.push(false);
+      }
+      $(".buttons-disabled").prop("disabled", true);
+    },
+    showAll: function() {
+      this.emptyAll();
+      for (var i = 0; i < 48; i++) {
+        this.priceList.push(11 + i * 0.1);
+        this.chargeList.push(i == 1 ? null : 11 + i * 0.1);
+        this.dischargeList.push(i == 2 ? null : 11 + i * 0.1);
+        this.autoList.push(i == 0 || i == 4 ? 11 + i * 0.1 : null);
+        if (i % 4 == 2 || i % 4 == 3) {
+          this.manualList.push(11 + i * 0.1);
+          this.manualOriginalList.push(11 + i * 0.1);
+        } else {
+          this.manualList.push(null);
+          this.manualOriginalList.push(null);
+        }
+        this.manualSelectedList.push(false);
+      }
+      this.checkOneHour();
+    },
     searchClick: function() {
       if (this.selectedCompany === '') {
-        alert('会社を入力してください。'); return;
+        alert('会社を入力してください。'); this.clearAll(); return;
       }
       if (this.selectedArea === '') {
-        alert('エリアを入力してください。'); return;
+        alert('エリアを入力してください。'); this.clearAll(); return;
       }
-    },
-    isBlank: function(v) {
-      return v === null || v === '';
+      $(".buttons-disabled").prop("disabled", false);
+      this.showAll();
     },
     checkAuto: function() {
-      for (var i = 0; i < 48; i++) {
-        var va = this.autoList[i];
-        var vm = this.manualList[i];
-        if (!this.isBlank(va) && !this.isBlank(vm)) {
-          $("#message").val('既に自動設定されている日時に対し、手動設定はできません。');
+      for (var i = 0; i < 48; i++)
+        if (this.autoList[i] && this.manualList[i])
           return false;
+      return true;
+    },
+    checkOneHour: function() {
+      for (var i = 0; i < 48; i++) {
+        $(".td-manual:nth-child(" + (i+1) + ")").removeClass("td-readonly");
+      }
+      var result = true;
+      for (var i = 0; i < 48; i++) {
+        var indexes = [];
+        if (i > 0) indexes.push(i - 1);
+        if (i > 1) indexes.push(i - 2);
+        if (i < 47) indexes.push(i + 1);
+        if (i < 46) indexes.push(i + 2);
+        for(var k = 0; k < indexes.length; k++) {
+          var j = indexes[k];
+          if (this.manualList[j]) {
+            if (!this.manualList[i]) {
+              $(".td-manual:nth-child(" + (i+1) + ")").addClass("td-readonly");
+            } else if (j === i - 2 || j === i + 2) {
+              $(".td-manual:nth-child(" + (i+1) + ")").addClass("td-readonly");
+              result = false;
+            }
+          }
         }
+      }
+      return result;
+    },
+    checkDRFormat: function() {
+      for (var i = 0; i < 48; i++)
+        if (this.manualList[i] && !/[+-]?(\d){1,4}(\.\d)?/.test(this.manualList[i]))
+          return false;
+      return true;
+    },
+    checkChargeEmpty: function() {
+      for (var i = 0; i < 48; i++)
+        if (!this.chargeList[i] && +this.manualList[i] > 0)
+          return false;
+      return true;
+    },
+    checkDischargeEmpty: function() {
+      for (var i = 0; i < 48; i++)
+        if (!this.dischargeList[i] && -this.manualList[i] > 0)
+          return false;
+      return true;
+    },
+    checkChargeTooMuch: function() {
+      for (var i = 0; i < 48; i++)
+        if (this.chargeList[i] && +this.manualList[i] > 0 && +this.manualList[i] > +this.chargeList[i])
+          return false;
+      return true;
+    },
+    checkDischargeTooMuch: function() {
+      for (var i = 0; i < 48; i++)
+        if (this.dischargeList[i] && -this.manualList[i] > 0 && -this.manualList[i] > +this.chargeList[i])
+          return false;
+      return true;
+    },
+    commonCheck: function() {
+      $("#message").val('');
+      if (!this.checkAuto()) {
+        $("#message").val('エラー：既に自動設定されている日時に対し、手動設定はできません。');
+        return false;
+      }
+      if (!this.checkDRFormat()) {
+        $("#message").val('エラー：設定できるDR量は-9999.9から9999.9の範囲です。');
+        return false;
+      }
+      if (!this.checkChargeEmpty()) {
+        $("#message").val('エラー：充電余力がない時間帯のＤＲ要求はできません。');
+        return false;
+      }
+      if (!this.checkChargeEmpty()) {
+        $("#message").val('エラー：充電余力がない時間帯のＤＲ要求はできません。');
+        return false;
+      }
+      if (!this.checkDischargeEmpty()) {
+        $("#message").val('エラー：放電余力がない時間帯のＤＲ要求はできません。');
+        return false;
+      }
+      if (!this.checkChargeTooMuch()) {
+        $("#message").val('エラー：手動設定したDR量が、充電余力のDR量を超過しています。');
+        return false;
+      }
+      if (!this.checkDischargeTooMuch()) {
+        $("#message").val('エラー：手動設定したDR量が、放電余力のDR量を超過しています。');
+        return false;
       }
       return true;
     },
-    check: function() {
-      $("#message").val('');
-      if (!this.checkAuto()) return false;
-    },
     checkClick: function() {
-      this.check();
+      this.commonCheck();
     },
     execClick: function() {
-      this.check();
+      this.commonCheck();
     },
     manualClick: function(event, index) {
       var $td = $(event.currentTarget);
@@ -82,38 +199,19 @@ var app = new Vue({
       $('.div-manual').not($div).prop('contenteditable', "false");
       if ($div.prop('contenteditable') !== "true") {
         $td.toggleClass("td-manual-selected");
-        app.manualSelectedList[index] = !app.manualSelectedList[index];
-        console.log(app.manualSelectedList[index]);
+        this.manualSelectedList[index] = !this.manualSelectedList[index];
+        console.log(this.manualSelectedList[index]);
         $div.prop('contenteditable', "true");
       }
     },
     manualBlur: function(event, index) {
       $('.div-manual').prop('contenteditable', "false");
       var $div = $(event.currentTarget);
-      app.manualList[index] = $div.html();
-      console.log(app.manualList[index]);
+      this.manualList[index] = $div.html();
+      console.log(this.manualList[index]);
     }
   }
 })
-for (var i = 0; i < 48; i++) {
-  app.timeList.push("00:00");
-  app.priceList.push(11 + i * 0.1);
-  app.chargeList.push(11 + i * 0.1);
-  app.dischargeList.push(11 + i * 0.1);
-  if (i == 0 || i == 4) {
-    app.autoList.push(11 + i * 0.1);
-  } else {
-    app.autoList.push(null);
-  }
-  if (i % 4 == 2 || i % 4 == 3) {
-    app.manualList.push(11 + i * 0.1);
-    app.manualOriginalList.push(11 + i * 0.1);
-  } else {
-    app.manualList.push(null);
-    app.manualOriginalList.push(null);
-  }
-  app.manualSelectedList.push(false);
-}
 app.companyList.push({id: 0, name: 'A会社'});
 app.companyList.push({id: 1, name: 'B会社'});
 app.areaList.push({id: 0, name: 'エリア1'});
@@ -126,6 +224,11 @@ app.areaList.push({id: 6, name: 'エリア7'});
 app.areaList.push({id: 7, name: 'エリア8'});
 app.areaList.push({id: 8, name: 'エリア9'});
 app.areaList.push({id: 9, name: 'エリア10'});
+for (var i = 0; i < 48; i++) {
+  var j = Math.floor(i / 2);
+  app.timeList.push((j < 10 ? "0": "") + j + ":" + (i % 2 == 0 ? "00" : "30"));
+}
+app.clearAll();
 
 $(function() {
   $('#date1').datepicker();
