edea7c9e5823a92a24c40d96004e3ed850956e05
趙偉 <zwvista@msn.com>
2018-09-27 01:27:50 +0900

    k

---
+++ CSharp/SampleCS/cc/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace cc
+{
+    class MainClass
+    {
+        public static async Task Main(string[] args)
+        {
+            Console.WriteLine("main start");
+            await AsyncMethod();
+            Console.WriteLine("main finish!");
+        }
+
+        static async Task AsyncMethod() {
+            Console.WriteLine("async start");
+            var result = await MyMethod();
+            Console.WriteLine("async finish");
+        }
+
+        static async Task<int> MyMethod() {
+            for (int i = 0; i < 5; i++) {
+                Console.WriteLine(i.ToString());
+                await Task.Delay(1000);
+            }
+            return 0;
+        }
+    }
+}
--- Java/Spring/SpringBootSecurity/src/main/java/com/example/demo/controller/HelloController.java
+++ Java/Spring/SpringBootSecurity/src/main/java/com/example/demo/controller/HelloController.java
@@ -4,14 +4,22 @@ import com.example.demo.domain.Fruit;
 import com.example.demo.mapper1.Fruit1Mapper;
 import com.example.demo.mapper2.Fruit2Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 
 @Controller
 public class HelloController {
+
+    @Value("${logfile}")
+    private String logfile;
 
     @Autowired
     Fruit1Mapper fruit1Mapper;
@@ -24,6 +32,19 @@ public class HelloController {
         List<Fruit> list2 = fruit2Mapper.selectAll();
         model.addAttribute("fruits", list);
         model.addAttribute("fruits2", list2);
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String name = auth.getName(); //get logged in username
+        try
+        {
+            FileWriter fw = new FileWriter(logfile,true); //the true will append the new data
+            fw.write(name +  " added a line\n");//appends the string to the file
+            fw.close();
+            System.out.println("added a line to " + logfile);
+        }
+        catch(IOException ioe)
+        {
+            System.err.println("IOException: " + ioe.getMessage());
+        }
         return "hello";
     }
 
