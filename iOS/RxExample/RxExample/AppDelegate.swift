bb94e87110ec08176a71536ded9c0a72f193f15d
趙偉 <zwvista@msn.com>
2018-10-23 11:48:39 +0900

    4.2

--- iOS/RxExample/RxExample/AppDelegate.swift
+++ iOS/RxExample/RxExample/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     let disposeBag = DisposeBag()
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         Post.getPostAsString().do(onNext: { print($0) }).subscribe().disposed(by: disposeBag)
         Post.getPostAsJson().do(onNext: { print($0.description) }).subscribe().disposed(by: disposeBag)
--- iOS/RxExample/RxExample/RestApi.swift
+++ iOS/RxExample/RxExample/RestApi.swift
@@ -12,15 +12,20 @@ import RxSwift
 import RxAlamofire
 
 // https://stackoverflow.com/questions/27855319/post-request-with-a-simple-string-in-body-with-alamofire
-extension String: ParameterEncoding {
+class StringEncoding: ParameterEncoding {
+    let body: String;
+    
+    public init(body: String) {
+        self.body = body
+    }
     
     public func encode(_ urlRequest: URLRequestConvertible, with parameters: Parameters?) throws -> URLRequest {
         var request = try urlRequest.asURLRequest()
-        request.httpBody = data(using: .utf8, allowLossyConversion: false)
+        request.httpBody = body.data(using: .utf8, allowLossyConversion: false)
         return request
     }
-    
 }
+
 extension Encodable {
     
     public func toJSONString(prettyPrint: Bool) throws -> String? {
@@ -41,10 +46,10 @@ class RestApi {
         return RxCodableAlamofire.object(.get, url, keyPath: keyPath)
     }
     static func update(url: String, body: String) -> Observable<String> {
-        return RxAlamofire.string(.put, url, encoding: body)
+        return RxAlamofire.string(.put, url, encoding: StringEncoding(body: body))
     }
     static func create(url: String, body: String) -> Observable<String> {
-        return RxAlamofire.string(.post, url, encoding: body)
+        return RxAlamofire.string(.post, url, encoding: StringEncoding(body: body))
     }
     static func delete(url: String) -> Observable<String> {
         return RxAlamofire.string(.delete, url)
