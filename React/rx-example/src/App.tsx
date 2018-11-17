ecd77b2b4680cedb2409dc3d098bcf1a2111473a
趙偉 <zwvista@msn.com>
2018-11-18 05:55:54 +0900

    number

--- React/rx-example/src/App.tsx
+++ React/rx-example/src/App.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
 import './App.css';
 
-import logo from './logo.svg';
 import { PostService } from './post.service';
 import { Inject, Module } from 'react.di';
+import { combineLatest, fromEvent, Observable } from 'rxjs';
+import { map, pluck, startWith } from 'rxjs/operators';
 
 @Module({
   providers: [
@@ -13,23 +14,61 @@ import { Inject, Module } from 'react.di';
 class App extends React.Component {
   @Inject postService!: PostService;
 
+  state = {
+    number1: '1',
+    number2: '2',
+    number3: '3',
+    result: '',
+    result2: '',
+  };
+
   componentDidMount() {
     console.log(this.postService);
+    const f = (id: string) => {
+      const e = document.getElementById(id) as HTMLInputElement;
+      return fromEvent(e, 'input').pipe<string, string>(pluck('target', 'value'), startWith(e.value)) as Observable<string>;
+    };
+    const g = (s: string) => Number(s) || 0;
+    combineLatest(f('number1'), f('number2'), f('number3'))
+      .pipe(map((results: string[]) => String(g(results[0]) + g(results[1]) + g(results[2]))))
+      .subscribe(result2 => this.setState({result2}));
+    this.onChangeNumber(null);
   }
 
-  public render() {
+  render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
+        <p>
+          <input name="number1" className="number" value={this.state.number1} onChange={this.onChangeNumber} /> +
+          <input name="number2" className="number" value={this.state.number2} onChange={this.onChangeNumber} /> +
+          <input name="number3" className="number" value={this.state.number3} onChange={this.onChangeNumber} /> =
+          <label>{this.state.result}</label>
+        </p>
+        <p>
+          <input id="number1" className="number" defaultValue="1" /> +
+          <input id="number2" className="number" defaultValue="2" /> +
+          <input id="number3" className="number" defaultValue="3" /> =
+          <label>{this.state.result2}</label>
         </p>
       </div>
     );
   }
+
+  onChangeNumber = (e: any) => {
+    const f = () => {
+      const g = (s: string) => Number(s) || 0;
+      this.setState({
+        result: String(g(this.state.number1) + g(this.state.number2) + g(this.state.number3))
+      });
+    };
+    if (e != null) {
+      const elem = e.nativeEvent.target as HTMLInputElement;
+      // https://stackoverflow.com/questions/30782948/why-calling-react-setstate-method-doesnt-mutate-the-state-immediately
+      this.setState({[elem.name]: elem.value}, () => f());
+    } else {
+      f();
+    }
+  };
 }
 
 export default App;
--- Vue/rx-example/src/App.vue
+++ Vue/rx-example/src/App.vue
@@ -1,10 +1,26 @@
 <template>
+  <div>
+    <p>
+      <input class="number" v-model="number1" @input="onChangeNumber()"> +
+      <input class="number" v-model="number2" @input="onChangeNumber()"> +
+      <input class="number" v-model="number3" @input="onChangeNumber()"> =
+      <label>{{result}}</label>
+    </p>
+    <p>
+      <input id="number1" class="number" value="1"> +
+      <input id="number2" class="number" value="2"> +
+      <input id="number3" class="number" value="3"> =
+      <label>{{result2}}</label>
+    </p>
+  </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import { PostService } from './post.service';
 import { inject } from 'vue-typescript-inject';
+import { combineLatest, fromEvent, Observable } from 'rxjs';
+import { map, pluck, startWith } from 'rxjs/operators';
 
 @Component({
   providers: [
@@ -13,8 +29,35 @@ import { inject } from 'vue-typescript-inject';
 })
 export default class App extends Vue {
   @inject() postService!: PostService;
+
+  number1 = '1';
+  number2 = '2';
+  number3 = '3';
+  result = '';
+  result2 = '';
+
+  mounted() {
+    const f = (id: string) => {
+      const e = document.getElementById(id) as HTMLInputElement;
+      return fromEvent(e, 'input').pipe<string, string>(pluck('target', 'value'), startWith(e.value)) as Observable<string>;
+    };
+    const g = (s: string) => Number(s) || 0;
+    combineLatest(f('number1'), f('number2'), f('number3'))
+      .pipe(map((results: string[]) => String(g(results[0]) + g(results[1]) + g(results[2]))))
+      .subscribe(result => this.result2 = result);
+    this.onChangeNumber();
+  }
+
+  onChangeNumber() {
+    const g = (s: string) => Number(s) || 0;
+    this.result = String(g(this.number1) + g(this.number2) + g(this.number3));
+  }
 }
 </script>
 
 <style>
+  .number {
+    width: 50px;
+    text-align: right;
+  }
 </style>
