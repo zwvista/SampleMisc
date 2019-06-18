6202b99d5a71c00420976e6becb21b8d371e02c6
趙偉 <zwvista@msn.com>
2019-06-18 11:57:12 +0900

    Revert "rx"
    This reverts commit 9fa9373c28abf579611a4d9b355b41b9b00ba379.

--- Cpp/RxExample/main.cpp
+++ Cpp/RxExample/main.cpp
@@ -1,10 +1,12 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <rxqt.hpp>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    rxqt::run_loop rxqt_run_loop;
     w.show();
 
     return a.exec();
