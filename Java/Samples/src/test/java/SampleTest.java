56783b451c85f43acf0e5386b805a1d9cb2eb835
趙偉 <zwvista@msn.com>
2019-10-14 13:48:36 +0900

    selenium

--- Java/Samples/src/test/java/SampleTest.java
+++ Java/Samples/src/test/java/SampleTest.java
@@ -1,4 +1,22 @@
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SampleTest {
+	@Test
+	public void testGoogleSearch() throws InterruptedException {
+	  // Optional, if not specified, WebDriver will search your path for chromedriver.
+	  System.setProperty("webdriver.chrome.driver", "/Applications/chromedriver");
 
+	  WebDriver driver = new ChromeDriver();
+	  driver.get("http://www.google.com/xhtml");
+	  Thread.sleep(5000);  // Let the user actually see something!
+	  WebElement searchBox = driver.findElement(By.name("q"));
+	  searchBox.sendKeys("ChromeDriver");
+	  searchBox.submit();
+	  Thread.sleep(5000);  // Let the user actually see something!
+	  driver.quit();
+	}
 }
