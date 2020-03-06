6376fee7faf519ef359efb921aae51890a7add5c
zwvista <zwvista@msn.com>
2020-03-06 13:23:01 +0900

    Fody

--- CSharp/RxExample/RxExample/AppViewModel.cs
+++ CSharp/RxExample/RxExample/AppViewModel.cs
@@ -1,46 +1,29 @@
 ﻿using ReactiveUI;
-using System;
+using ReactiveUI.Fody.Helpers;
 
 namespace RxExample
 {
     public class AppViewModel : ReactiveObject
     {
-        private string _number1;
-        public string Number1
-        {
-            get { return _number1; }
-            set { this.RaiseAndSetIfChanged(ref _number1, value); }
-        }
+        [Reactive]
+        public string Number1 { get; set; } = "1";
 
-        private string _number2;
-        public string Number2
-        {
-            get { return _number2; }
-            set { this.RaiseAndSetIfChanged(ref _number2, value); }
-        }
+        [Reactive]
+        public string Number2 { get; set; } = "2";
 
-        private string _number3;
-        public string Number3
-        {
-            get { return _number3; }
-            set { this.RaiseAndSetIfChanged(ref _number3, value); }
-        }
+        [Reactive]
+        public string Number3 { get; set; } = "3";
 
-        private readonly ObservableAsPropertyHelper<string> _result;
-        public string Result => _result.Value;
+        public string Result { [ObservableAsProperty] get; }
 
         public AppViewModel()
         {
-            _number1 = "1"; _number2 = "2"; _number3 = "3";
-
             int f(string s) =>
                 int.TryParse(s, out var o) ? o : 0;
 
-            _result = this.WhenAnyValue(x => x.Number1, x => x.Number2, x => x.Number3,
+            this.WhenAnyValue(x => x.Number1, x => x.Number2, x => x.Number3,
                 (s1, s2, s3) => (f(s1) + f(s2) + f(s3)).ToString())
-                .ToProperty(this, x => x.Result, "");
+                .ToPropertyEx(this, x => x.Result);
         }
-
     }
-
 }
