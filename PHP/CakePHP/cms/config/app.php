e33fa8794cd13cc16d2f3e4ff606b249f0ec65e1
趙偉 <zwvista@msn.com>
2020-05-06 16:02:27 +0900

    cms

--- PHP/CakePHP/cms/config/app.php
+++ PHP/CakePHP/cms/config/app.php
@@ -270,43 +270,14 @@ return [
          * in app_local.php depending on the applications needs.
          */
         'default' => [
-            'className' => Connection::class,
-            'driver' => Mysql::class,
+            'className' => 'Cake\Database\Connection',
+            'driver' => 'Cake\Database\Driver\Mysql',
             'persistent' => false,
+            'host' => 'localhost',
+            'database' => 'cake_cms',
+            'encoding' => 'utf8mb4',
             'timezone' => 'UTC',
-
-            /**
-             * For MariaDB/MySQL the internal default changed from utf8 to utf8mb4, aka full utf-8 support, in CakePHP 3.6
-             */
-            //'encoding' => 'utf8mb4',
-
-            /**
-             * If your MySQL server is configured with `skip-character-set-client-handshake`
-             * then you MUST use the `flags` config to set your charset encoding.
-             * For e.g. `'flags' => [\PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8mb4']`
-             */
-            'flags' => [],
             'cacheMetadata' => true,
-            'log' => false,
-
-            /*
-             * Set identifier quoting to true if you are using reserved words or
-             * special characters in your table or column names. Enabling this
-             * setting will result in queries built using the Query Builder having
-             * identifiers quoted when creating SQL. It should be noted that this
-             * decreases performance because each query needs to be traversed and
-             * manipulated before being executed.
-             */
-            'quoteIdentifiers' => false,
-
-            /*
-             * During development, if using MySQL < 5.6, uncommenting the
-             * following line could boost the speed at which schema metadata is
-             * fetched from the database. It can also be set directly with the
-             * mysql configuration directive 'innodb_stats_on_metadata = 0'
-             * which is the recommended value in production environments
-             */
-            //'init' => ['SET GLOBAL innodb_stats_on_metadata = 0'],
         ],
 
         /*
---
+++ PHP/CakePHP/cms/src/Model/Table/ArticlesTable.php
@@ -0,0 +1,39 @@
+<?php
+// in src/Model/Table/ArticlesTable.php
+namespace App\Model\Table;
+
+use Cake\ORM\Table;
+// the Text class
+use Cake\Utility\Text;
+// the EventInterface class
+use Cake\Event\EventInterface;
+// the Validator class
+use Cake\Validation\Validator;
+
+class ArticlesTable extends Table
+{
+    public function initialize(array $config): void
+    {
+        $this->addBehavior('Timestamp');
+    }
+    public function beforeSave(EventInterface $event, $entity, $options)
+    {
+        if ($entity->isNew() && !$entity->slug) {
+            $sluggedTitle = Text::slug($entity->title);
+            // trim slug to maximum length defined in schema
+            $entity->slug = substr($sluggedTitle, 0, 191);
+        }
+    }
+    public function validationDefault(Validator $validator): Validator
+    {
+        $validator
+            ->allowEmptyString('title', false)
+            ->minLength('title', 10)
+            ->maxLength('title', 255)
+    
+            ->allowEmptyString('body', false)
+            ->minLength('body', 10);
+    
+        return $validator;
+    }    
+}
