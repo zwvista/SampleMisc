30ecb565a9f1d0a602183fa18838325be7a98fd5
zwvista <zwvista@msn.com>
2020-05-21 19:16:33 +0900

    TimeSpanUpDown

---
+++ CSharp/RxExample/RxExample/TimeSpanUpDown.xaml.cs
@@ -0,0 +1,60 @@
+﻿using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+using System;
+using System.Reactive.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace RxExample
+{
+    /// <summary>
+    /// TimeSpanUpDown.xaml の相互作用ロジック
+    /// </summary>
+    public partial class TimeSpanUpDown : UserControl
+    {
+        public TimeSpanUpDownViewModel VM { get; } = new TimeSpanUpDownViewModel();
+
+        public int Value
+        {
+            get => (int)GetValue(ValueProperty);
+            set => SetValue(ValueProperty, value);
+        }
+
+        public static readonly DependencyProperty ValueProperty =
+            DependencyProperty.Register("Value", typeof(int), typeof(TimeSpanUpDown),
+                new FrameworkPropertyMetadata(0, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, ValuePropertyChangedCallback));
+
+        private static void ValuePropertyChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var control = d as TimeSpanUpDown;
+            control.VM.SetValue(control.Value);
+        }
+
+        public TimeSpanUpDown()
+        {
+            InitializeComponent();
+            grid.DataContext = VM;
+            VM.ValueChanged.Subscribe(value => SetValue(ValueProperty, value));
+        }
+    }
+    public class TimeSpanUpDownViewModel : ReactiveObject
+    {
+        [Reactive]
+        public int Hour { get; set; }
+        [Reactive]
+        public int Minute { get; set; }
+        [Reactive]
+        public int Second { get; set; }
+        public IObservable<int> ValueChanged;
+        public void SetValue(int value)
+        {
+            Hour = value / 3600;
+            Minute = value / 60 % 60;
+            Second = value % 60;
+        }
+        public TimeSpanUpDownViewModel()
+        {
+            ValueChanged = this.WhenAnyValue(x => x.Hour, x => x.Minute, x => x.Second, (h, m, s) => h * 3600 + m * 60 + s);
+        }
+    }
+}
