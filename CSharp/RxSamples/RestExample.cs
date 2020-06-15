c7e5395b23bd65543f1aa84a9980bfad487579c7
zwvista <zwvista@msn.com>
2020-06-15 18:40:29 +0900

    restsharp

--- CSharp/RxSamples/RestExample.cs
+++ CSharp/RxSamples/RestExample.cs
@@ -11,6 +11,8 @@ using System.Reactive.Threading.Tasks;
 
 using Newtonsoft.Json;
 using Plugin.Connectivity;
+using RestRequest = RestSharp.Serializers.Newtonsoft.Json.RestRequest;
+using RestSharp;
 
 namespace RxSamples
 {
@@ -37,26 +39,20 @@ namespace RxSamples
         };
 
         public async Task<string> GetPostAsString(int id) =>
-        !CrossConnectivity.Current.IsConnected ? null : await client.GetStringAsync($"posts/{id}");
+            await client.GetStringAsync($"posts/{id}");
 
         public async Task<Post> GetPostAsJson(int id)
         {
-            if (!CrossConnectivity.Current.IsConnected) return null;
-
             var json = await client.GetStringAsync($"posts/{id}");
             var item = await Task.Run(() => JsonConvert.DeserializeObject<Post>(json));
-
             return item;
         }
 
         public async Task<IEnumerable<Post>> GetPosts(int n)
         {
             IEnumerable<Post> items = new List<Post>();
-            if (CrossConnectivity.Current.IsConnected)
-            {
-                var json = await client.GetStringAsync("posts");
-                items = await Task.Run(() => JsonConvert.DeserializeObject<IEnumerable<Post>>(json));
-            }
+            var json = await client.GetStringAsync("posts");
+            items = await Task.Run(() => JsonConvert.DeserializeObject<IEnumerable<Post>>(json));
             return items.Take(n);
         }
 
@@ -65,37 +61,23 @@ namespace RxSamples
 
         public async Task<Post> CreatePost(Post item)
         {
-            if (item == null || !CrossConnectivity.Current.IsConnected) return null;
-
             var response = await client.PostAsync("posts", GetStringContent(item));
-            if (!response.IsSuccessStatusCode) return null;
-
             var json = await response.Content.ReadAsStringAsync();
             var item2 = await Task.Run(() => JsonConvert.DeserializeObject<Post>(json));
-
             return item2;
         }
 
         public async Task<Post> UpdatePost(Post item)
         {
-            if (item == null || !CrossConnectivity.Current.IsConnected) return null;
-
             var response = await client.PutAsync($"posts/{item.Id}", GetStringContent(item));
-            if (!response.IsSuccessStatusCode) return null;
-
             var json = await response.Content.ReadAsStringAsync();
             var item2 = await Task.Run(() => JsonConvert.DeserializeObject<Post>(json));
-
             return item2;
         }
 
         public async Task<string> DeletePost(int id)
         {
-            if (!CrossConnectivity.Current.IsConnected) return null;
-
             var response = await client.DeleteAsync($"posts/{id}");
-            if (!response.IsSuccessStatusCode) return null;
-
             var json = await response.Content.ReadAsStringAsync();
             return json;
         }
@@ -109,16 +91,13 @@ namespace RxSamples
         };
 
         public IObservable<string> GetPostAsString(int id) =>
-        !CrossConnectivity.Current.IsConnected ? Observable.Empty<string>() :
             client.GetStringAsync($"posts/{id}").ToObservable();
 
         public IObservable<Post> GetPostAsJson(int id) =>
-        !CrossConnectivity.Current.IsConnected? Observable.Empty<Post>() :
             client.GetStringAsync($"posts/{id}").ToObservable()
             .Select(json => JsonConvert.DeserializeObject<Post>(json));
 
         public IObservable<Post> GetPosts(int n) =>
-        !CrossConnectivity.Current.IsConnected ? Observable.Empty<Post>() :
             client.GetStringAsync("posts").ToObservable()
             .SelectMany(json => JsonConvert.DeserializeObject<IEnumerable<Post>>(json))
             .Take(n);
@@ -127,24 +106,59 @@ namespace RxSamples
         new StringContent(JsonConvert.SerializeObject(item), Encoding.UTF8, "application/json");
 
         public IObservable<Post> CreatePost(Post item) =>
-        item == null || !CrossConnectivity.Current.IsConnected ? Observable.Empty<Post>() :
             client.PostAsync("posts", GetStringContent(item)).ToObservable()
-            .SelectMany(response => !response.IsSuccessStatusCode ? Observable.Empty<Post>() :
-                response.Content.ReadAsStringAsync().ToObservable()
+            .SelectMany(response => response.Content.ReadAsStringAsync().ToObservable()
                 .Select(json => JsonConvert.DeserializeObject<Post>(json)));
 
         public IObservable<Post> UpdatePost(Post item) =>
-        item == null || !CrossConnectivity.Current.IsConnected? Observable.Empty<Post>() :
             client.PutAsync($"posts/{item.Id}", GetStringContent(item)).ToObservable()
-            .SelectMany(response => !response.IsSuccessStatusCode? Observable.Empty<Post>() :
-                response.Content.ReadAsStringAsync().ToObservable()
+            .SelectMany(response => response.Content.ReadAsStringAsync().ToObservable()
                 .Select(json => JsonConvert.DeserializeObject<Post>(json)));
 
         public IObservable<string> DeletePost(int id) =>
-        !CrossConnectivity.Current.IsConnected ? Observable.Empty<string>() :
             client.DeleteAsync($"posts/{id}").ToObservable()
-            .SelectMany(response => !response.IsSuccessStatusCode ? Observable.Empty<string>() :
-                response.Content.ReadAsStringAsync().ToObservable());
+            .SelectMany(response => response.Content.ReadAsStringAsync().ToObservable());
+    }
+
+    public class PostDataStoreByRestSharp
+    {
+        protected RestClient client = new RestClient("https://jsonplaceholder.typicode.com/");
+        public IObservable<string> GetPostAsString(int id)
+        {
+            var request = new RestRequest($"posts/{id}", Method.GET);
+            return client.ExecuteAsync<string>(request).ToObservable().Select(o => o.Data);
+        }
+        public IObservable<Post> GetPostAsJson(int id)
+        {
+            var request = new RestRequest($"posts/{id}", Method.GET);
+            return client.ExecuteAsync<Post>(request).ToObservable().Select(o => o.Data);
+        }
+
+        public IObservable<Post> GetPosts(int n)
+        {
+            var request = new RestRequest($"posts", Method.GET);
+            return client.ExecuteAsync<IEnumerable<Post>>(request).ToObservable().SelectMany(o => o.Data).Take(n);
+        }
+
+        public IObservable<Post> CreatePost(Post item)
+        {
+            var request = new RestRequest($"posts", Method.POST);
+            request.AddJsonBody(item);
+            return client.ExecuteAsync<Post>(request).ToObservable().Select(o => o.Data);
+        }
+
+        public IObservable<Post> UpdatePost(Post item)
+        {
+            var request = new RestRequest($"posts/{item.Id}", Method.PUT);
+            request.AddJsonBody(item);
+            return client.ExecuteAsync<Post>(request).ToObservable().Select(o => o.Data);
+        }
+
+        public IObservable<string> DeletePost(int id)
+        {
+            var request = new RestRequest($"posts/{id}", Method.DELETE);
+            return client.ExecuteAsync<string>(request).ToObservable().Select(o => o.Data);
+        }
     }
 
     public class RestExample
@@ -194,6 +208,28 @@ namespace RxSamples
                 dataStore.DeletePost(1).Subscribe(Console.WriteLine);
                 Console.ReadKey();
             }
-        }
+            {
+                var dataStore = new PostDataStoreByRestSharp();
+                dataStore.GetPostAsString(1).Subscribe(Console.WriteLine);
+                dataStore.GetPostAsJson(1).Subscribe(Console.WriteLine);
+                dataStore.GetPosts(2).Do(Console.WriteLine).Subscribe();
+                dataStore.CreatePost(new Post
+                {
+                    UserId = 101,
+                    Id = 0,
+                    Title = "test title",
+                    Body = "test body"
+                }).Subscribe(Console.WriteLine);
+                dataStore.UpdatePost(new Post
+                {
+                    UserId = 101,
+                    Id = 1,
+                    Title = "test title",
+                    Body = "test body"
+                }).Subscribe(Console.WriteLine);
+                dataStore.DeletePost(1).Subscribe(Console.WriteLine);
+                Console.ReadKey();
+            }
+        }   
     }
 }
