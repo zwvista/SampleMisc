15404e3f5106d14f27d98efb0252578665f42419
趙偉 <zwvista@msn.com>
2020-11-30 06:07:56 +0900

    lm 2

--- Swift/Test/Landmarks/Landmarks/ContentView.swift
+++ Swift/Test/Landmarks/Landmarks/ContentView.swift
@@ -9,8 +9,30 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+            MapView()
+                .edgesIgnoringSafeArea(.top)
+                .frame(height: 300)
+
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                HStack {
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("California")
+                        .font(.subheadline)
+                }
+            }
             .padding()
+
+            Spacer()
+        }
     }
 }
 
