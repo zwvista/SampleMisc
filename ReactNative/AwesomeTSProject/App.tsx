c83991afe9c22077adf3e6aaee105ab36fb6f8d1
趙　偉 <zwvista@msn.com>
2021-02-01 18:16:45 +0900

    drawer

--- ReactNative/AwesomeTSProject/App.tsx
+++ ReactNative/AwesomeTSProject/App.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react';
 import { Button, View } from 'react-native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { DrawerActions, NavigationContainer, useNavigation } from '@react-navigation/native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
+// https://stackoverflow.com/questions/60316864/react-navigation-drawer-v5
+// https://stackoverflow.com/questions/60233339/react-native-hamburger-onpress-issue
 function HomeScreen({ navigation }:any) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
@@ -25,56 +27,53 @@ function NotificationsScreen({ navigation }:any) {
 }
 
 const Drawer = createDrawerNavigator();
-
-function draw() {
-  return (
-
-      <Drawer.Navigator initialRouteName="Home" >
-        <Drawer.Screen name="Home" component={HomeScreen} 
-        options={{
-        drawerIcon: () => <Ionicons name='md-home' size={30} color='#130f40' />,
-          }}
-         />
-        <Drawer.Screen name="Notifications" component={NotificationsScreen}
-        options={{
-        drawerIcon: () => <Ionicons name='md-notifications' size={30} color='#130f40' />,
-          }}
-         />
-      </Drawer.Navigator>
-
-  );
-}
-
-
-
 const Stack = createStackNavigator();
 
-function Def(){
+const DrawerComponent = () => {
+  return (
+    <Drawer.Navigator initialRouteName="Home">
+      <Drawer.Screen name="Home" component={HomeScreen} 
+        options={{
+          drawerIcon: () => <Ionicons name='md-home' size={30} color='#130f40' />,
+        }}
+      />
+      <Drawer.Screen name="Notifications" component={NotificationsScreen} 
+        options={{
+          drawerIcon: () => <Ionicons name='md-notifications' size={30} color='#130f40' />,
+        }}
+      />
+    </Drawer.Navigator>
+  );
+};
+
+export default () => {
   return (
     <NavigationContainer>
-    <Stack.Navigator>
-    <Stack.Screen
-        name="Home"
-        component={draw}
-        options={{
-          title: 'My home',
-          headerStyle: {
-            backgroundColor: '#5f27cd',
-          },
-          headerTintColor: '#fff',
-          headerTitleStyle: {
-            fontWeight: 'bold',
-          },
-
-          headerLeft: ({navigation}) =>  (<Ionicons name='md-menu' style={{paddingLeft: "20px"}} size={30} color='white' onPress={() => navigation.toggleDrawer()} />)
-
-
-        }}
-
-      />
-    </Stack.Navigator>
-    </NavigationContainer>    
-  )
-}
-
-export default Def;
+      <Stack.Navigator>
+        <Stack.Screen
+          options={{
+            title: 'My home',
+            headerStyle: {
+              backgroundColor: '#5f27cd',
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+            },
+            headerLeft: () => {
+              const navigation = useNavigation();
+              return (
+                <Ionicons name='md-menu' style={{paddingLeft: "4%"}} size={30} color='white' onPress={() => navigation.dispatch(DrawerActions.openDrawer())} />
+              );
+            }
+          }}
+          component={DrawerComponent}
+          name="Drawer"
+        />
+        {/*
+         * Rest Screens
+         */}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
