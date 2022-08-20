deecea1f28880694ae7c4e62989c0b86496fcb77
趙偉 <>
2022-08-20 15:23:00 +0900

    numbers

--- Swift/SwiftUIExample/SwiftUIExample/ContentView.swift
+++ Swift/SwiftUIExample/SwiftUIExample/ContentView.swift
@@ -8,36 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
-    @StateObject var vm = NumbersViewModel()
+    @State var page : Int? = 0
     var body: some View {
-        VStack {
-            HStack {
-                Text("")
-                    .modifier(LeftModifier())
-                TextField("Number1", text: $vm.number1)
-                    .modifier(RightModifier())
+        NavigationView {
+            VStack {
+                NavigationLink(destination: NumbersView(),
+                    tag: 1, selection: $page) { EmptyView() }
+                Button(action : {
+                   page = 1
+                }){
+                   Text("Add Numbers")
+                }
             }
-            HStack {
-                Text("")
-                    .modifier(LeftModifier())
-                TextField("Number2", text: $vm.number2)
-                    .modifier(RightModifier())
-            }
-            HStack {
-                Text("+")
-                    .modifier(LeftModifier())
-                TextField("Number3", text: $vm.number3)
-                    .modifier(RightModifier())
-            }
-            Divider()
-            HStack {
-                Text("")
-                    .modifier(LeftModifier())
-                TextField("Result", text: $vm.result)
-                    .disabled(true)
-                    .modifier(RightModifier())
-            }
-        }.frame(width: 200, height: 200, alignment: .center)
+        }
     }
 }
 
