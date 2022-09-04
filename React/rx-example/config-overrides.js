8e2f1887e473d7dc039df82dec4352eecab761e8
趙偉 <>
2022-09-04 21:45:41 +0900

    plugin

---
+++ React/rx-example/config-overrides.js
@@ -0,0 +1,12 @@
+const {
+  override,
+  addBabelPlugins
+} = require("customize-cra");
+
+module.exports = override(
+    addBabelPlugins(
+        "babel-plugin-transform-typescript-metadata",
+        ["@babel/plugin-proposal-decorators", { "legacy": true }],
+        ["@babel/plugin-proposal-class-properties", { "loose": true }],
+    )
+)
