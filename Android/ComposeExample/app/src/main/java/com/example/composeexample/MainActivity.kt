ac55460e280358ecca1bb09897aa90a2c1b2e9c4
趙偉 <>
2022-09-19 12:12:22 +0900

    remember

--- Android/ComposeExample/app/src/main/java/com/example/composeexample/MainActivity.kt
+++ Android/ComposeExample/app/src/main/java/com/example/composeexample/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
@@ -55,7 +56,8 @@ fun RightTextField(
 }
 
 @Composable
-fun AddNumbers(vm: NumbersViewModel = NumbersViewModel()) {
+fun AddNumbers() {
+    val vm = remember { NumbersViewModel() }
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
