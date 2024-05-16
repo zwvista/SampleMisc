4d7b8ca2bcdc2cfa3e48b259070e393c06e6f496
zwvista <zwvista@msn.com>
2024-05-17 05:01:03 +0900

    SampleCS

---
+++ CSharp/SampleCS/SampleCS/PatternMatching.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleCS
+{
+    internal class PatternMatching
+    {
+        // constant pattern (number)
+        // relational pattern
+        // logical pattern
+        public void Match1()
+        {
+            var x = new Random().Next(10);
+            var y = x switch
+            {
+                5 => "constant",
+                < 7 and > 3 => "and",
+                not (> 8 or < 2) => "or",
+                _ => "else"
+            };
+            Console.WriteLine($"{x},{y}");
+        }
+    }
+}
--- CSharp/SampleCS/SampleCS/Program.cs
+++ CSharp/SampleCS/SampleCS/Program.cs
@@ -1,38 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using SampleCS;
 
-using SamplePad;
-
-namespace SampleCS
-{
-    static class Program
-    {
-        /// <summary>
-        /// アプリケーションのメイン エントリ ポイントです。
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-
-            List<SampleHarness> harnesses = new List<SampleHarness>();
-
-            // LinqSamples:
-            var ixsample = new IxSample();
-            harnesses.Add(ixsample);
-
-            // LinqSamples:
-            var zwsample = new ZWSample();
-            harnesses.Add(zwsample);
-
-            using (SampleForm form = new SampleForm("SampleCS", harnesses))
-            {
-                form.ShowDialog();
-            }
-        }
-    }
-}
+var pm = new PatternMatching();
+pm.Match1();
